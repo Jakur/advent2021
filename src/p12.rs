a6de48dc6dce514bfeb6de3b7318518caf02c31b
Justin Kur <Justink97@hotmail.com>
2021-12-12 02:22:28 -0500

    Optimize visited detection on day 12

--- src/p12.rs
+++ src/p12.rs
@@ -4,28 +4,29 @@ use std::collections::HashMap;
 const START: usize = 0;
 const END: usize = 1;
 
+#[derive(Clone)]
 struct Visit {
     current_node: usize,
-    history: Vec<u8>,
-    free_long_visit: bool,
+    history: u32,
+    long_visits: bool, // Long visits remaining
 }
 
 impl Visit {
-    fn new(current_node: usize, mut history: Vec<u8>, mut free_long_visit: bool) -> Self {
-        if current_node < history.len() {
-            history[current_node] += 1;
-            if history[current_node] == 2 && current_node >= END + 1 {
-                free_long_visit = false;
-            }
+    fn new(current_node: usize, mut history: u32, mut long_visits: bool) -> Self {
+        let check = 1 << current_node;
+        if long_visits && current_node >= (END + 1) && (check & history == check) {
+            long_visits = false;
         }
+        history |= check;
         Self {
             current_node,
             history,
-            free_long_visit,
+            long_visits,
         }
     }
-    fn max_visit_len(&self) -> u8 {
-        self.free_long_visit as u8 + 1
+    fn index(&self, index: usize) -> bool {
+        let shifted = 1 << index;
+        (shifted & self.history) == shifted
     }
 }
 
@@ -47,9 +48,6 @@ impl<'a> Graph<'a> {
             length,
         }
     }
-    pub fn is_upper(&self, node: usize) -> bool {
-        node >= self.lower.len()
-    }
     pub fn get_index(&self, node: &str) -> usize {
         if let Some(num) = self.lower.get(node) {
             *num
@@ -64,41 +62,35 @@ impl<'a> Graph<'a> {
         self.adj_matrix[idx1 * self.length + idx2] = true;
         self.adj_matrix[idx2 * self.length + idx1] = true;
     }
-    pub fn explore(&self) -> usize {
-        let mut visit_vec = vec![0u8; self.lower.len()];
-        visit_vec[0] += 1; // Cannot revisit the first point;
-        let mut stack: Vec<Visit> = vec![Visit::new(0, visit_vec, false)];
+    pub fn part1(&self) -> usize {
+        let visit_vec = 1; // Cannot revisit the first point
+        let stack: Vec<Visit> = vec![Visit::new(0, visit_vec, false)];
+        self.explore(stack)
+    }
+    pub fn part2(&self) -> usize {
+        let visit_vec = 1; // Cannot revisit the first point
+        let stack: Vec<Visit> = vec![Visit::new(0, visit_vec, true)];
+        self.explore(stack)
+    }
+    pub fn explore(&self, mut stack: Vec<Visit>) -> usize {
         let mut count = 0;
         while let Some(val) = stack.pop() {
             if val.current_node == END {
                 count += 1;
                 continue;
             }
-            for idx in 0..self.length {
+            for idx in 1..self.lower.len() {
                 if self.adj_matrix[self.length * val.current_node + idx]
-                    && *val.history.get(idx).unwrap_or(&0) == 0
+                    && (val.long_visits || !val.index(idx))
                 {
-                    stack.push(Visit::new(idx, val.history.clone(), false))
+                    stack.push(Visit::new(idx, val.history.clone(), val.long_visits))
                 }
             }
-        }
-        count
-    }
-    pub fn explore2(&self) -> usize {
-        let mut visit_vec = vec![0u8; self.lower.len()];
-        visit_vec[0] += 1; // Cannot revisit the first point;
-        let mut stack: Vec<Visit> = vec![Visit::new(0, visit_vec, true)];
-        let mut count = 0;
-        while let Some(val) = stack.pop() {
-            if val.current_node == END {
-                count += 1;
-                continue;
-            }
-            for idx in 0..self.length {
-                if self.adj_matrix[self.length * val.current_node + idx]
-                    && (*val.history.get(idx).unwrap_or(&0)) < val.max_visit_len()
-                {
-                    stack.push(Visit::new(idx, val.history.clone(), val.free_long_visit))
+            for idx in self.lower.len()..self.length {
+                if self.adj_matrix[self.length * val.current_node + idx] {
+                    let mut next_visit = val.clone();
+                    next_visit.current_node = idx;
+                    stack.push(next_visit)
                 }
             }
         }
@@ -133,8 +125,7 @@ pub fn solve(input: &str) -> Solution<u32, u32> {
         let n2 = sp.next().unwrap();
         graph.set_edge(n1, n2);
     }
-    // dbg!(graph.adj_matrix.iter().filter(|&&x| x == true).count());
-    Solution::new(graph.explore() as u32, graph.explore2() as u32)
+    Solution::new(graph.part1() as u32, graph.part2() as u32)
 }
 
 fn str_map_insert<'a>(s: &'a str, map: &mut HashMap<&'a str, usize>) {
